3735bc920e4b1af3f4107865b267212c8d3d126d
Ashley Benntt <ashleyben21@gmail.com>
2019-06-19 11:14:29 +0100

    Updates

---
+++ precache-manifest.ac114f6e36cc765620d9371abcd13993.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f028c95466ed0b125ffec28f73393a",
+    "url": "/REACT-Adress-Book/index.html"
+  },
+  {
+    "revision": "e60fb92d9fc35cc7cd7e",
+    "url": "/REACT-Adress-Book/static/css/main.c046a828.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/REACT-Adress-Book/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e60fb92d9fc35cc7cd7e",
+    "url": "/REACT-Adress-Book/static/js/main.4ae5ca13.chunk.js"
+  },
+  {
+    "revision": "157ef5c609addb05682d",
+    "url": "/REACT-Adress-Book/static/js/runtime~main.0f9ac088.js"
+  }
+]);
